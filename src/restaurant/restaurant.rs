4e46906c1d9f756dc8bb3cd5419001c337ce8a64
betester <ikramzulkarnaen02@gmail.com>
2022-07-27 08:58:38 +0700

    feat: add method boilerplate on restaurant

--- src/restaurant/restaurant.rs
+++ src/restaurant/restaurant.rs
@@ -12,6 +12,39 @@ pub struct Restaurant {
 }
 
 impl Restaurant {
+
+    fn add_food_menu(&self) {
+
+    }
+
+    fn remove_food_menu(&self) {
+
+    }
+
+    fn add_customers(&self) {
+
+    }
+
+    fn remove_customers(&self) {
+
+    }
+
+    fn add_chefs(&self) {
+
+    }
+
+    fn remove_chefs(&self) {
+
+    }
+
+    fn add_waiters(&self) {
+
+    }
+
+    fn remove_waiters(&self) {
+
+    }
+
     fn is_visitable(&self) -> bool {
     // Handle whether the available_seats is fulfilling
     // Handle if the waiters is fulfilling
@@ -19,6 +52,10 @@ impl Restaurant {
         false
     }
 
+
     
-    
+
+    pub fn set_rating(&mut self, rating: String) {
+        self.rating = rating;
+    }
 }
