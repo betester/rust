38589fa43f09c5f74aab82c0b26d3708fc701638
betester <ikramzulkarnaen02@gmail.com>
2022-07-27 09:07:30 +0700

    feat: add menu attribute and method boilerplate

--- src/restaurant/menu.rs
+++ src/restaurant/menu.rs
@@ -1,3 +1,15 @@
+use super::food::Food;
+
 pub struct Menu {
-    
+    foods : Vec<Food>
+}
+
+impl Menu {
+    fn add_food(&self) {
+        
+    }
+
+    fn remove_food(&self) {
+        
+    }
 }
