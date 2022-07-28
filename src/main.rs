01de7fd130919589eb180eb0eeaf7701ad371ce2
betester <ikramzulkarnaen02@gmail.com>
2022-07-28 09:28:30 +0700

    feat: add boilerplate on main file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
+use crate::restaurant::{customer::Customer, chef::Chef};
+
 pub mod restaurant;
 
 fn main() {
-    println!("Hello world!");
+    let mut customers = HashMap::<String,Customer>::new();
+    let mut chefs = HashMap::<String,Chef>::new();
+    let mut restaurants = HashMap::<String,Chef>::new();
+
+}
+
+fn create_customer() {
+
+}
+
+fn create_chef() {
+
+}
+
+fn create_restaurant() {
+    
 }
 
